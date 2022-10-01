cbb1fcce5d0b1141058597cc0116aea83060adca
pauline-shen <s9633291@gmail.com>
2022-10-01 02:38:21 -0400

    styled navebar, profile section, compose tweet portion with only char count functionality.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  const tweet = document.getElementById("tweet-text");
+  tweet.addEventListener("input", () => {
+    const maxLen = 140;
+    let len = this.getElementById("tweet-text").value.length;
+    let c = this.getElementById("counter");
+    c.value = maxLen - len;
+    if (c.value >= 0) {
+      document.getElementById("counter").className = "valid";
+    } else {
+      document.getElementById("counter").className = "invalid";
+    }
+  });
+});
+
